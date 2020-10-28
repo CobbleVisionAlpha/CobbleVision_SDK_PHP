76f00461ff5e30b9994576d777f9740d2eb1b85e
Cob @ CobbleVision <73602547+CobbleVisionAlpha@users.noreply.github.com>
2020-10-28 18:41:56 +0100

    Update CobbleVision_API.php

--- CobbleVision_API.php
+++ CobbleVision_API.php
@@ -79,7 +79,8 @@ function setDebugging(debugBool){
 function uploadMediaFile(price_category, publicBool, name, tags, file){
   try{
     $endpoint = "media"
-    if(substr(($this->BaseURL)-1) != "/"{
+      
+    if(substr(($this->BaseURL)-1) != "/"){
       throw new Exception("Cobble BaseURL must end on slash!")
     }
        
@@ -138,7 +139,8 @@ function uploadMediaFile(price_category, publicBool, name, tags, file){
 function deleteMediaFile(IDArray){
   try{
     $endpoint = "media"
-    if(substr(($this->BaseURL)-1) != "/"{
+      
+    if(substr(($this->BaseURL)-1) != "/"){
       throw new Exception("Cobble BaseURL must end on slash!")
     }
        
@@ -157,6 +159,7 @@ function deleteMediaFile(IDArray){
     }
       
     $invalidMediaIDList = checkIDArrayForInvalidValues($IDArray)
+    
     if(count($invalidMediaIDList) > 0){
       throw new Exception("You provided invalid Media IDs. Please check your input!")
     }
@@ -164,7 +167,7 @@ function deleteMediaFile(IDArray){
     #Unfortunately PHP does not support filebuffers or UInt8Array
     $client = new GuzzleHTTP\Client()
       
-    $res = $client -> request("POST", ($this -> BaseURL) + $endpoint + "?id=" + json_encode(IDArray), ["headers" => array("Accept" => "application/json", "Content-Type" => "application/json"), "auth" => array($this -> apiUserName => $this -> apiPassword)])
+    $res = $client -> request("DELETE", ($this -> BaseURL) + $endpoint + "?id=" + json_encode($IDArray), ["headers" => array("Accept" => "application/json", "Content-Type" => "application/json"), "auth" => array($this -> apiUserName => $this -> apiPassword)])
     
     if(($this -> debugging) == True){
       echo $res -> getStatusCode()
@@ -194,7 +197,8 @@ function deleteMediaFile(IDArray){
 function launchCalculation(algorithms, media, type, notificationURL){
   try{
     $endpoint = "calculation"
-    if(substr(($this->BaseURL)-1) != "/"{
+      
+    if(substr(($this->BaseURL)-1) != "/"){
       throw new Exception("Cobble BaseURL must end on slash!")
     }
 
@@ -228,8 +232,15 @@ function launchCalculation(algorithms, media, type, notificationURL){
     
     #Unfortunately PHP does not support filebuffers or UInt8Array
     $client = new GuzzleHTTP\Client()
+   
+    $jsonObject = array(
+       "algorithms" => $algorithms,
+       "media" => $media,
+       "type" => $type,
+       "notificationURL" => $notificationURL,
+    )  
       
-    $res = $client -> request("POST", ($this -> BaseURL) + $endpoint + "?id=" + json_encode(IDArray), ["headers" => array("Accept" => "application/json", "Content-Type" => "application/json"), "auth" => array($this -> apiUserName => $this -> apiPassword)])
+    $res = $client -> request("POST", ($this -> BaseURL), ["headers" => array("Accept" => "application/json", "Content-Type" => "application/json"), "auth" => array($this -> apiUserName => $this -> apiPassword), "json" => $jsonObject]])
     
     if(($this -> debugging) == True){
       echo $res -> getStatusCode()
@@ -251,20 +262,202 @@ function launchCalculation(algorithms, media, type, notificationURL){
 # @async
 # @function waitForCalculationCompletion() 
 # @param {array} calculationIDArray Array of Calculation ID's
-# @returns {Response} This returns the WaitForCalculationResponse. The body is in JSON format.  
+# @returns {Response} This returns the WaitForCalculationResponse. The body is in JSON format.   
+
+function waitForCalculationCompletion(calculationIDArray){
+  try{
+    $endpoint = "calculation"
+      
+    if(substr(($this->BaseURL)-1) != "/"){
+      throw new Exception("Cobble BaseURL must end on slash!")
+    }
+
+    $keyArray = array(1 => "algorithms", 2 => "media", 3 => "type", 4 => "notificationURL", 5 => "Your API Username", 6 => "Your Api Token")
+    $valueArray = array(1 => $algorithms, 2 => $media, 3 => $type, 4 => $notificationURL, 5 => ($this -> apiUserName), 6 => ($this -> apiToken))
+    $typeArray = array(1 => "array", 2 => "array", 3 => "string", 4 => "string", 5 => "string", 6 => "string")   
+       
+    try{
+      checkTypeOfParameter($valueArray, $typeArray)
+    }catch(Exception $e){
+      $err_message = intval($e -> GetMessage())
+      if(gettype($err_message) != "Integer"){
+         throw new Exception("The provided data is not valid: " + $keyArray[$err_message] + " is not of type " + $typeArray[$err_message])
+      }else{
+        throw new Exception($e -> GetMessage())
+    }
+      
+    if(!array_search[$type, $valid_job_types]){
+      throw new Exception("Calculation Type is not valid!")
+    }
+    
+    $invalidAlgorithmIDs = checkIDArrayForInvalidValues($algorithms)
+    if(count($invalidAlgorithmIDs) > 0){
+      throw new Exception("You provided invalid Algorithm IDs. Please check your input!")
+    }
+      
+    $invalidMediaIDs = checkIDArrayForInvalidValues($media)
+    
+    if(count($invalidMediaIDs) > 0){
+      throw new Exception("You provided invalid Media IDs. Please check your input!")
+    }
+    
+    $calculationFinishedBool = False
+    
+      while($calculationFinishedBool == False){
+    
+      #Unfortunately PHP does not support filebuffers or UInt8Array
+      $client = new GuzzleHTTP\Client()
+      $res = $client -> request("GET", ($this -> BaseURL) + $endpoint + "?id=" + json_encode($calculationIDArray), ["headers" => array("Accept" => "application/json", "Content-Type" => "application/json"), "auth" => array($this -> apiUserName => $this -> apiPassword)])
+    
+      $result = $res -> getBody()
+      $result = json_decode($result)
+      
+      if(is_array($result -> getBody())){
+        for($i = 0; $i < count($result); $i++){
+          if(property_exists($result[i]), "status"))}
+            if($result[i] -> status === "finished"){
+              calculationFinishedBool = True
+            }else
+              calculationFinishedBool = False
+              break;
+            }
+          }
+        }
+      }else{
+        if(property_exists($result, "error")){
+          $calculationFinishedBool = True
+        }
+      }
+  
+      if(calculationFinishedBool == False){
+        sleep(3)
+      }
+    }
+  
+    if(($this -> debugging) == True){
+      echo $res -> getStatusCode()
+      echo $res -> getBody()
+    }
+    
+    return $res
+   }catch(Exception $e){
+      
+     if($this -> debugging){
+       echo ($e -> GetMessage())
+     }
+      
+     throw new Exception($e -> GetMessage())
+   }   
+}
 
 # This function deletes Result Files or calculations in status "waiting" from CobbleVision. You cannot delete finished jobs beyond their result files, as we keep them for billing purposes.
 # @async
 # @function deleteCalculation()
 # @param {array} IDArray Array of ID's as Strings
 # @returns {Response} This returns the DeleteCalculationResponse. The body is in JSON format.
-
+       
+function deleteCalculation(IDArray){
+  try{
+    $endpoint = "calculation"
+      
+    if(substr(($this->BaseURL)-1) != "/"){
+      throw new Exception("Cobble BaseURL must end on slash!")
+    }
+       
+    $keyArray = array(1 => "IDArray", 2 => "Your API Username", 3 => "Your Api Token")
+    $valueArray = array(1 => $IDArray, 2 => ($this -> apiUserName), 3 => ($this -> apiToken))
+    $typeArray = array(1 => "array", 2 => "string", 3 => "string")
+       
+    try{
+      checkTypeOfParameter($valueArray, $typeArray)
+    }catch(Exception $e){
+      $err_message = intval($e -> GetMessage())
+      if(gettype($err_message) != "Integer"){
+         throw new Exception("The provided data is not valid: " + $keyArray[$err_message] + " is not of type " + $typeArray[$err_message])
+      }else{
+        throw new Exception($e -> GetMessage())
+    }
+      
+    $invalidCalculationList = checkIDArrayForInvalidValues($IDArray)
+      
+    if(count($invalidCalculationList) > 0){
+      throw new Exception("You provided invalid Calculation IDs. Please check your input!")
+    }
+    
+    #Unfortunately PHP does not support filebuffers or UInt8Array
+    $client = new GuzzleHTTP\Client()
+      
+    $res = $client -> request("DELETE", ($this -> BaseURL) + $endpoint + "?id=" + json_encode($IDArray) + "&returnOnlyStatusBool=True", ["headers" => array("Accept" => "application/json", "Content-Type" => "application/json"), "auth" => array($this -> apiUserName => $this -> apiPassword)])
+    
+    if(($this -> debugging) == True){
+      echo $res -> getStatusCode()
+      echo $res -> getBody()
+    }
+    
+    return $res
+   }catch(Exception $e){
+      
+     if($this -> debugging){
+       echo ($e -> GetMessage())
+     }
+      
+     throw new Exception($e -> GetMessage())
+   }   
+}       
+       
 # Get Calculation Result with CobbleVision's Web API. Returns a response object with body, response and headers properties, deducted from npm request module;
 # @async
 # @function getCalculationResult()
-# @param {array} idArray ID of calculation to return result Array 
+# @param {array} IDArray ID of calculation to return result Array 
 # @param {boolean} returnOnlyStatusBool Return full result or only status? See Doc for more detailed description!
 # @returns {Response} This returns the GetCalculationResult. The body is in json format.
+
+function getCalculationResult(IDArray, returnOnlyStatusBool){
+  try{
+    $endpoint = "calculation"
+      
+    if(substr(($this->BaseURL)-1) != "/"){
+      throw new Exception("Cobble BaseURL must end on slash!")
+    }
+       
+    $keyArray = array(1 => "IDArray", 2 => "returnOnlyStatusBool", 3 => "Your API Username", 4 => "Your Api Token")
+    $valueArray = array(1 => $IDArray, 2 => $returnOnlyStatusBool, 3 => ($this -> apiUserName), 4 => ($this -> apiToken))
+    $typeArray = array(1 => "array", 2 => "boolean", 3 => "string", 4 => "string")
+       
+    try{
+      checkTypeOfParameter($valueArray, $typeArray)
+    }catch(Exception $e){
+      $err_message = intval($e -> GetMessage())
+      if(gettype($err_message) != "Integer"){
+         throw new Exception("The provided data is not valid: " + $keyArray[$err_message] + " is not of type " + $typeArray[$err_message])
+      }else{
+        throw new Exception($e -> GetMessage())
+    }
+    
+    $invalidCalculationList = checkIDArrayForInvalidValues($IDArray)
+      
+    if(count($invalidCalculationList) > 0){
+      throw new Exception("You provided invalid Calculation IDs. Please check your input!")
+    }    
+    
+    $client = new GuzzleHTTP\Client()
+    $res = $client -> request("GET", ($this -> BaseURL) + $endpoint + "?id=" json_encode($IDArray) + "&returnOnlyStatusBool=" + json_encode($returnOnlyStatusBool), ["headers" => array("Accept" => "application/json", "Content-Type" => "application/json"), "auth" => array($this -> apiUserName => $this -> apiPassword)])
+    
+    if(($this -> debugging) == True){
+      echo $res -> getStatusCode()
+      echo $res -> getBody()
+    }
+    
+    return $res
+   }catch(Exception $e){
+      
+     if($this -> debugging){
+       echo ($e -> GetMessage())
+     }
+      
+     throw new Exception($e -> GetMessage())
+   }   
+}
 
 # Request your calculation result by ID with the CobbleVision API. Returns a response object with body, response and headers properties, deducted from npm request module;
 # @async
@@ -275,4 +468,51 @@ function launchCalculation(algorithms, media, type, notificationURL){
 # @param {number} height target height of visualization file
 # @returns {Response} This returns the GetCalculationVisualization Result. The body is in binary format.
 
+function getCalculationVisualization(id, returnBase64Bool, width, height){
+  try{
+    $endpoint = "calculation/visualization"
+      
+    if(substr(($this->BaseURL)-1) != "/"){
+      throw new Exception("Cobble BaseURL must end on slash!")
+    }
+       
+    $keyArray = array(1 => "IDArray", 2 => "returnOnlyStatusBool", 3 => "Your API Username", 4 => "Your Api Token")
+    $valueArray = array(1 => $IDArray, 2 => $returnOnlyStatusBool, 3 => ($this -> apiUserName), 4 => ($this -> apiToken))
+    $typeArray = array(1 => "array", 2 => "boolean", 3 => "string", 4 => "string")
+       
+    try{
+      checkTypeOfParameter($valueArray, $typeArray)
+    }catch(Exception $e){
+      $err_message = intval($e -> GetMessage())
+      if(gettype($err_message) != "Integer"){
+         throw new Exception("The provided data is not valid: " + $keyArray[$err_message] + " is not of type " + $typeArray[$err_message])
+      }else{
+        throw new Exception($e -> GetMessage())
+    }
+    
+    $invalidCalculationList = checkIDArrayForInvalidValues($IDArray)
+      
+    if(count($invalidCalculationList) > 0){
+      throw new Exception("You provided invalid Calculation IDs. Please check your input!")
+    }    
+    
+    $client = new GuzzleHTTP\Client()
+    $res = $client -> request("GET", ($this -> BaseURL) + $endpoint + "?id=" json_encode($IDArray) + "&returnOnlyStatusBool=" + json_encode($returnOnlyStatusBool), ["headers" => array("Accept" => "application/json", "Content-Type" => "application/json"), "auth" => array($this -> apiUserName => $this -> apiPassword)])
+    
+    if(($this -> debugging) == True){
+      echo $res -> getStatusCode()
+      echo $res -> getBody()
+    }
+    
+    return $res
+   }catch(Exception $e){
+      
+     if($this -> debugging){
+       echo ($e -> GetMessage())
+     }
+      
+     throw new Exception($e -> GetMessage())
+   }   
+} 
+  
 >
