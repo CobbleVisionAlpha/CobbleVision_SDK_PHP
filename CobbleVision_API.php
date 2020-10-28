0257aaeca24008325097f82740987f89cc992b6a
Cob @ CobbleVision <73602547+CobbleVisionAlpha@users.noreply.github.com>
2020-10-28 16:58:58 +0100

    Create CobbleVision_API.php

---
+++ CobbleVision_API.php
@@ -0,0 +1,13 @@
+<?php
+
+$environmentType = True
+public $serverAdress = "https://cobblevision.com"
+
+public $valid_price_categories = array(1 => "high", 2 => "medium", 3 => "low")
+public $valid_job_types = array(1 => "QueuedJob")
+
+public $debugging = False
+
+[....]
+
+>
